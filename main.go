9f24c5f994aa2231d2ea6412ae0840112a792a09
Ludovic Borie <ludovic@karnott.fr>
2025-01-20 09:05:40 +0100

    feat: need a backend, migrate to appengine

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const estamitechRss = "https://feeds.zencastr.com/f/bOMlUWx6.rss"
+
+type Rss struct {
+	Channel Channel `xml:"channel"`
+}
+
+type Channel struct {
+	Items []Item `xml:"item"`
+}
+
+type Item struct {
+	Title       string `xml:"title"`
+	PubDate     string `xml:"pubDate"`
+	Description string `xml:"description"`
+	Image       struct {
+		Href string `xml:"href,attr"`
+	} `xml:"itunes_image"`
+}
+
+func main() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		http.ServeFile(writer, request, "index.html")
+	})
+	http.HandleFunc("/rss", func(writer http.ResponseWriter, request *http.Request) {
+		response, err := http.DefaultClient.Get(estamitechRss)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer func(Body io.ReadCloser) {
+			_ = Body.Close()
+		}(response.Body)
+
+		respBytes, err := io.ReadAll(response.Body)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		resp := string(respBytes)
+		resp = strings.ReplaceAll(resp, "itunes:", "itunes_")
+		var rss Rss
+		err = xml.Unmarshal([]byte(resp), &rss)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		// Marshal rss to json
+		itemsJson, err := json.Marshal(rss.Channel.Items)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		writer.Header().Set("Content-Type", "application/json")
+		// Access cors policy
+		//writer.Header().Set("Access-Control-Allow-Origin", "*")
+		//writer.Header().Set("Access-Control-Allow-Methods", "GET,OPTIONS")
+		writer.WriteHeader(http.StatusOK)
+		_, _ = writer.Write(itemsJson)
+	})
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+	_ = http.ListenAndServe(":"+port, nil)
+}
